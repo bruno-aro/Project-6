40ab4551e74580f2800299a9fff9c6350484acbd
brunoaro <brunoagustinaro@gmail.com>
2025-10-13 16:10:39 +0200

    psycopg

--- streamlit/streamlitBA_app.py
+++ streamlit/streamlitBA_app.py
@@ -1,25 +1,5 @@
 import streamlit as st
 import psycopg
+import pandas as pd
 
-st.title("🟠 BTC News")
-
-def get_btc(author=None):
-    dbconn = st.secrets["DBCONN"]
-    conn = psycopg.connect(dbconn)
-
- cur.execute("SELECT * FROM btc_data WHERE author = %s", (author,))
-        data = cur.fetchall()
-    
-    cur.close()
-    conn.close()
-    
-    return data
-
-author = st.selectbox(
-    "Filter by author (optional):",
-    options=[None, "Alex Dovbnya", "Denys Serhiiichuk", "Arman Shirinyan"],
-    index=0
-)
-
-df = get_btc(author)
-st.dataframe(df, use_container_width=True)
+st.title("🍉 Hello World!!!!!!")
