2f719c795642068dcbee8ec1370f2e328ab0ec5d
brunoaro <brunoagustinaro@gmail.com>
2025-10-13 21:47:34 +0200

    testing stremlit

--- streamlit/streamlitBA_app.py
+++ streamlit/streamlitBA_app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 import psycopg
-import pandas as pd
 
-st.title("🍉 Hello World!!!!!!")
+def get_data(author):
+    dbconn = st.secrets["DBCONN"]
+    conn = psycopg.connect(dbconn)
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM btc_data WHERE author = %s", (author,))
+    data = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+    
+    return data
+    
+
+st.title("Bitcoin Information Center 🪙")
+
+option = st.selectbox(
+    "Which author would you like to read?",
+    ("Denys Serhiichuk", "Arman Shirinyan", "Godfrey Benjamin"),
+    index=None
+)
+
+st.write("You selected:", option)
+
+if option is not None:
+    data = get_data(option)
+    st.dataframe(data)
